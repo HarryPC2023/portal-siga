78ac0e6ab95c7720cb7278b7211176fe7880e14f
Harry <harrypc2021@hotmail.com>
2026-08-11 21:56:51 -0500

    nuevo

--- intranotas/sw.js
+++ intranotas/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Cache-First para assets estáticos
    ============================================================ */
 
-const CACHE_NAME = 'intranotas-v61';
+const CACHE_NAME = 'intranotas-v62';
 
 const ARCHIVOS_A_CACHEAR = [
     './index.html',
